20000a7b0692d69f43a18e5228545f536c569111
bocephus <itx2@outlook.com>
2019-08-11 12:38:48 -0700

    Add files via upload

---
+++ BackupScript_From_Destination.ps1
@@ -0,0 +1,9 @@
+﻿# prompts user for the data they would like to copy.  This will be entered whent the script is ran
+$loc = read-host "What is the file path of the data you would like to back up?"
+
+# Prompts user for the destination of where to copy the data to.  This will also be entered when
+# the Script is ran. 
+$dest = Read-Host "Where you would you like to save your data?"
+
+# actual execution of the script.  
+copy-item $loc\* -Recurse $dest -Verbose
